1928765dd0cfc6646de8bcc7363f1699f6f5081d
dlugi57 <dlugoszpeter@gmail.com>
2020-08-31 23:55:04 +0200

    Map json data to java objects

--- src/main/java/com/safetynet/alerts/service/DataServiceImpl.java
+++ src/main/java/com/safetynet/alerts/service/DataServiceImpl.java
@@ -1,13 +1,23 @@
 package com.safetynet.alerts.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.safetynet.alerts.model.Firestation;
+import com.safetynet.alerts.model.MedicalRecord;
 import com.safetynet.alerts.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.io.DataInput;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Wrapper;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -16,29 +26,29 @@ public class DataServiceImpl implements DataService {
     public void init() {
 
         try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("data.json")) {
-            //pass InputStream to JSON-Library, e.g. using Jackson
+
             ObjectMapper mapper = new ObjectMapper();
+            JsonNode jsonNode = mapper.readTree(in);
 
-            // convert JSON file to map
-            Map<?, ?> map = mapper.readValue(in, Map.class);
+            List<Person> persons = mapper.convertValue(jsonNode.get("persons"), ArrayList.class);
 
-            // print map entries
-            for (Map.Entry<?, ?> entry : map.entrySet()) {
-                System.out.println(entry.getKey() + "=" + entry.getValue());
-            }
+            List<Firestation> fireStations = mapper.convertValue(jsonNode.get("firestations"), ArrayList.class);
 
+            List<MedicalRecord> medicalRecords = mapper.convertValue(jsonNode.get("medicalrecords"), ArrayList.class);
 
-            /*
-            Person person = mapper.readValue(in, Person.class);
-            String jsonString = mapper.writeValueAsString(person);
+            String jsonString = mapper.writeValueAsString(persons);
             System.out.println(jsonString);
-            */
+
+            String jsonString1 = mapper.writeValueAsString(fireStations);
+            System.out.println(jsonString1);
+
+            String jsonString2 = mapper.writeValueAsString(medicalRecords);
+            System.out.println(jsonString2);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-
     }
-
 
 }

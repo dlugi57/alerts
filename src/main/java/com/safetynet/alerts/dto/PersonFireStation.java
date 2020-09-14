7931ee921c62eb0ba5b7e6968b6755267ba391cd
dlugi57 <dlugoszpeter@gmail.com>
2020-09-14 23:00:13 +0200

    BUG FIX create fire station person to finish first firestation endpoint with right data

---
+++ src/main/java/com/safetynet/alerts/dto/PersonFireStation.java
@@ -0,0 +1,65 @@
+package com.safetynet.alerts.dto;
+
+public class PersonFireStation {
+    //prénom, nom, adresse, numéro de téléphone
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String phone;
+
+
+    public PersonFireStation(String firstName, String lastName, String address, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public PersonFireStation() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonFireStation{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}

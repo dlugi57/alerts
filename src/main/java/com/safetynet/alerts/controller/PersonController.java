5604577262cbdf260d831b5c7d4f9e3a6d5e53c5
dlugi57 <dlugoszpeter@gmail.com>
2020-09-17 22:56:38 +0200

    CLEAN & COMMENT

--- src/main/java/com/safetynet/alerts/controller/PersonController.java
+++ src/main/java/com/safetynet/alerts/controller/PersonController.java
@@ -1,6 +1,5 @@
 package com.safetynet.alerts.controller;
 
-
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,58 +15,108 @@ import java.util.List;
 
 @RestController
 public class PersonController {
-    @Autowired
+
+    // init of person service
     PersonService personService;
 
-    //get swagger to this
-    //@ApiOperation(value= "Recupere un produit selon son ID")
+    /**
+     * Person controller constructor
+     *
+     * @param personService Person service initialization
+     */
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
 
+    /**
+     * Get person by first and last name
+     *
+     * @param firstName First name
+     * @param lastName  Last name
+     * @return Person object
+     */
     @GetMapping(value = "person")
-    public Person getPersonByFirstNameAndLastName(@RequestParam(required = true) String firstName, String lastName) throws ResponseStatusException {
-
+    public Person getPersonByFirstNameAndLastName(@RequestParam() String firstName, String lastName) {
+        // get person
         Person person = personService.getPersonByFirstNameAndLastName(firstName, lastName);
+        // if person don't exist send error message
         if (person == null)
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Person named  " + firstName + " " + lastName + " don't exist");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Person named " + firstName + " " + lastName + " don't exist");
 
         return person;
     }
 
+    /**
+     * Create person
+     *
+     * @param person person object with necessary data
+     * @return status and uri in header to the new created person
+     */
     @PostMapping(value = "/person")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Void> addPerson(@Valid @RequestBody Person person) {
 
+        // if person already exist send error message
         if (!personService.addPerson(person)) {
-            throw new ResponseStatusException(HttpStatus.FOUND, "This person already exist");
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "This person already exist");
         }
 
+        // create url with new created person
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/").queryParam("firstName", person.getFirstName()).queryParam("lastName", person.getLastName()).build().toUri();
         return ResponseEntity.created(location).build();
     }
 
+    /**
+     * Update person
+     *
+     * @param person person object
+     * @return status and uri to the updated person
+     */
     @PutMapping(value = "/person")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Void> updatePerson(@Valid @RequestBody Person person) {
 
+        //if person don't exist send error message with status
         if (!personService.updatePerson(person)) {
-            throw new ResponseStatusException(HttpStatus.FOUND, "This person don't exist");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This person don't exist");
         }
-
+        // create url with updated person
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/").queryParam("firstName", person.getFirstName()).queryParam("lastName", person.getLastName()).build().toUri();
         return ResponseEntity.created(location).build();
     }
 
+    /**
+     * Delete person
+     *
+     * @param person Person object with obligatory parameters
+     */
     @DeleteMapping(value = "/person")
     @ResponseStatus(HttpStatus.OK)
     public void deletePerson(@Valid @RequestBody Person person) {
 
+        //if person don't exist send error message with status
         if (!personService.deletePerson(person)) {
-            throw new ResponseStatusException(HttpStatus.FOUND, "This person don't exist");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "This person don't exist");
         }
     }
 
+    /**
+     * Get all persons from data
+     *
+     * @return List of persons
+     */
     @GetMapping(value = "persons")
     public List<Person> getPersons() {
-        return personService.getPersons();
+
+        List<Person> persons = personService.getPersons();
+
+        // if there is no persons in data base send error message
+        if (persons == null || persons.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "There is no persons in the data base");
+        }
+
+        return persons;
     }
 
 

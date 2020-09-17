4244fc766244e5bd5ea69e3d6b0ee1a79b1d2b6b
dlugi57 <dlugoszpeter@gmail.com>
2020-09-17 10:25:48 +0200

    RELEASE tests configuration

---
+++ src/test/java/com/safetynet/alerts/AgeCalculatorTest.java
@@ -0,0 +1,20 @@
+package com.safetynet.alerts;
+
+import com.safetynet.alerts.util.AgeCalculator;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class AgeCalculatorTest {
+
+    @Test
+    public void testCalculateAge_Success() {
+        // setup
+        LocalDate birthDate = LocalDate.of(1961, 5, 17);
+        // exercise
+        int actual = AgeCalculator.calculateAge(birthDate);
+        // assert
+        Assert.assertEquals(59, actual);
+    }
+}

4f8e69b6a261175f00609c35226dea7b8e5c70e1
dlugi57 <dlugoszpeter@gmail.com>
2020-09-19 23:16:52 +0200

    CLEAN & COMMENT

--- src/main/java/com/safetynet/alerts/dao/MedicalRecordDaoImpl.java
+++ src/main/java/com/safetynet/alerts/dao/MedicalRecordDaoImpl.java
@@ -8,35 +8,60 @@ import org.springframework.stereotype.Component;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+/**
+ * Initialization of medical records data with all crud methods
+ */
 @Component
 public class MedicalRecordDaoImpl implements MedicalRecordDao {
 
+    // list of medical records
     private List<MedicalRecord> medicalRecordsList;
 
+    /**
+     * Initialize all medical records
+     *
+     * @param dataService all data from file
+     */
     @Autowired
     public MedicalRecordDaoImpl(DataService dataService) {
         medicalRecordsList = dataService.getDataAlert().getMedicalrecords();
     }
 
+    /**
+     * get all medical records
+     *
+     * @return List of medical records
+     */
     @Override
     public List<MedicalRecord> getMedicalRecords() {
         return medicalRecordsList;
     }
 
-
+    /**
+     * Get medical record by first and last name
+     *
+     * @param firstName first name
+     * @param lastName  last name
+     * @return medical record object
+     */
     @Override
     public MedicalRecord getMedicalRecordByFirstNameAndLastName(String firstName, String lastName) {
         for (MedicalRecord medicalRecord : medicalRecordsList) {
+            // check if first name and last name are the same without considering capitals
             if (medicalRecord.getFirstName().equalsIgnoreCase(firstName) && medicalRecord.getLastName().equalsIgnoreCase(lastName)) {
                 return medicalRecord;
             }
-
         }
 
         return null;
     }
 
-
+    /**
+     * Add medical record
+     *
+     * @param medicalRecord medical record object
+     * @return true if success
+     */
     @Override
     public boolean addMedicalRecord(MedicalRecord medicalRecord) {
 
@@ -45,13 +70,19 @@ public class MedicalRecordDaoImpl implements MedicalRecordDao {
         return true;
     }
 
-
+    /**
+     * Update medical record
+     *
+     * @param medicalRecord medical record object
+     * @return true if success
+     */
     @Override
     public boolean updateMedicalRecord(MedicalRecord medicalRecord) {
         for (MedicalRecord existingMedicalRecord : medicalRecordsList) {
+            // check at firs obligatory parameters
             if (existingMedicalRecord.getFirstName().equalsIgnoreCase(medicalRecord.getFirstName())
                     && existingMedicalRecord.getLastName().equalsIgnoreCase(medicalRecord.getLastName())) {
-
+                // update only send data and if is null leave existing data
                 if (medicalRecord.getBirthdate() != null)
                     existingMedicalRecord.setBirthdate(medicalRecord.getBirthdate().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
                 if (medicalRecord.getAllergies() != null)
@@ -66,6 +97,12 @@ public class MedicalRecordDaoImpl implements MedicalRecordDao {
         return false;
     }
 
+    /**
+     * Delete medical record
+     *
+     * @param medicalRecord medical record object
+     * @return true if success
+     */
     @Override
     public boolean deleteMedicalRecord(MedicalRecord medicalRecord) {
         for (MedicalRecord existingMedicalRecord : medicalRecordsList) {
@@ -74,7 +111,6 @@ public class MedicalRecordDaoImpl implements MedicalRecordDao {
                 medicalRecordsList.remove(existingMedicalRecord);
                 return true;
             }
-
         }
         return false;
     }

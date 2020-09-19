88f8a269e146e468337fd52aac960b969b0fff55
dlugi57 <dlugoszpeter@gmail.com>
2020-09-19 23:32:26 +0200

    CLEAN & COMMENT

--- src/main/java/com/safetynet/alerts/dao/PersonDaoImpl.java
+++ src/main/java/com/safetynet/alerts/dao/PersonDaoImpl.java
@@ -1,9 +1,5 @@
 package com.safetynet.alerts.dao;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-import com.safetynet.alerts.model.FireStation;
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.service.DataService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,25 +8,46 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Initialization of person data with all crud methods
+ */
 @Component
 public class PersonDaoImpl implements PersonDao {
 
-    private List<Person> personList;
+    // list of persons
+    final private List<Person> personList;
 
+    /**
+     * Init list of person from global data
+     *
+     * @param dataService all data from file
+     */
     @Autowired
-   public PersonDaoImpl(DataService dataService) {
+    public PersonDaoImpl(DataService dataService) {
         personList = dataService.getDataAlert().getPersons();
     }
 
-
+    /**
+     * Get all persons
+     *
+     * @return list of all persons
+     */
     @Override
     public List<Person> getPersons() {
         return personList;
     }
 
+    /**
+     * Get person by first name and last name
+     *
+     * @param firstName first name
+     * @param lastName  last name
+     * @return person object
+     */
     @Override
     public Person getByFirstNameAndLastName(String firstName, String lastName) {
         for (Person person : personList) {
+            // check if first name and last name are the same without considering capitals
             if (person.getFirstName().equalsIgnoreCase(firstName) && person.getLastName().equalsIgnoreCase(lastName)) {
                 return person;
             }
@@ -40,6 +57,12 @@ public class PersonDaoImpl implements PersonDao {
         return null;
     }
 
+    /**
+     * Add person
+     *
+     * @param person person object
+     * @return true if success
+     */
     @Override
     public boolean addPerson(Person person) {
 
@@ -48,16 +71,24 @@ public class PersonDaoImpl implements PersonDao {
         return true;
     }
 
+    /**
+     * Update person
+     *
+     * @param person person object
+     * @return true when success
+     */
     @Override
     public boolean updatePerson(Person person) {
         for (Person existingPerson : personList) {
+            // check at firs obligatory parameters
             if (existingPerson.getFirstName().equalsIgnoreCase(person.getFirstName())
                     && existingPerson.getLastName().equalsIgnoreCase(person.getLastName())) {
-                if (person.getAddress() != null)existingPerson.setAddress(person.getAddress());
-                if (person.getCity() != null)existingPerson.setCity(person.getCity());
-                if (person.getEmail() != null)existingPerson.setEmail(person.getEmail());
-                if (person.getPhone() != null)existingPerson.setPhone(person.getPhone());
-                if (person.getZip() != null)existingPerson.setZip(person.getZip());
+                // update only send data and if is null leave existing data
+                if (person.getAddress() != null) existingPerson.setAddress(person.getAddress());
+                if (person.getCity() != null) existingPerson.setCity(person.getCity());
+                if (person.getEmail() != null) existingPerson.setEmail(person.getEmail());
+                if (person.getPhone() != null) existingPerson.setPhone(person.getPhone());
+                if (person.getZip() != null) existingPerson.setZip(person.getZip());
                 return true;
             }
 
@@ -65,9 +96,16 @@ public class PersonDaoImpl implements PersonDao {
         return false;
     }
 
+    /**
+     * Delete person
+     *
+     * @param person person object
+     * @return true when success
+     */
     @Override
     public boolean deletePerson(Person person) {
         for (Person existingPerson : personList) {
+            // check if first name and last name are the same without considering capitals
             if (existingPerson.getFirstName().equalsIgnoreCase(person.getFirstName())
                     && existingPerson.getLastName().equalsIgnoreCase(person.getLastName())) {
                 personList.remove(existingPerson);
@@ -78,44 +116,47 @@ public class PersonDaoImpl implements PersonDao {
         return false;
     }
 
+    /**
+     * Get list of persons by address
+     *
+     * @param address address of person
+     * @return List of persons
+     */
     @Override
     public List<Person> getPersonsByAddress(String address) {
-
-        List<Person> filteredPersonsList = new ArrayList<Person>();
+        // initialize list of persons to send
+        List<Person> filteredPersonsList = new ArrayList<>();
 
         for (Person person : personList) {
-
+            // check if address is the same without considering capitals and white spaces
             if (person.getAddress().replaceAll("\\s+", "").equalsIgnoreCase(address.replaceAll("\\s+", ""))) {
-
                 filteredPersonsList.add(person);
-
             }
+        }
 
-        }
-        if (!filteredPersonsList.isEmpty()){
-            return filteredPersonsList;
-        }
-        return null;
+        return filteredPersonsList;
     }
 
+    /**
+     * Get list of persons by city
+     *
+     * @param city city of person
+     * @return List of persons
+     */
     @Override
     public List<Person> getPersonsByCity(String city) {
 
-        List<Person> filteredPersonsList = new ArrayList<Person>();
+        // initialize list of persons to send
+        List<Person> filteredPersonsList = new ArrayList<>();
 
         for (Person person : personList) {
-
+            // check if city is the same without considering capitals and white spaces
             if (person.getCity().replaceAll("\\s+", "").equalsIgnoreCase(city.replaceAll("\\s+", ""))) {
-
                 filteredPersonsList.add(person);
-
             }
+        }
 
-        }
-        if (!filteredPersonsList.isEmpty()){
-            return filteredPersonsList;
-        }
-        return null;
+        return filteredPersonsList;
     }
 
 

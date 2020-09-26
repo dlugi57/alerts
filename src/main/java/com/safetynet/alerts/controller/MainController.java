bfbc59320e1ad8a2fa9798b5bc0a566049d8a887
dlugi57 <dlugoszpeter@gmail.com>
2020-09-26 23:57:48 +0200

    Logging

--- src/main/java/com/safetynet/alerts/controller/MainController.java
+++ src/main/java/com/safetynet/alerts/controller/MainController.java
@@ -3,6 +3,8 @@ package com.safetynet.alerts.controller;
 import com.safetynet.alerts.dto.*;
 import com.safetynet.alerts.service.FireStationService;
 import com.safetynet.alerts.service.PersonService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,9 @@ import java.util.List;
  */
 @RestController
 public class MainController {
+
+    static final Logger logger = LogManager
+            .getLogger(MainController.class);
 
     // initialize necessary services
     FireStationService fireStationService;
@@ -45,8 +50,13 @@ public class MainController {
 
         PersonsInFireStationArea personsInFireStationArea = fireStationService.getPersonsInFireStationArea(stationNumber);
         // if there is no persons in area send status and error message
-        if (personsInFireStationArea == null)
+        if (personsInFireStationArea == null) {
+            logger.error("GET firestation -> getPersonsInFireStationArea /**/ " +
+                    "HttpStatus : " + HttpStatus.NOT_FOUND + " /**/ Message : Fire station with this id " + stationNumber + " don't exist or there is no persons on this area");
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Fire station with this id " + stationNumber + " don't exist or there is no persons on this area");
+        }
+        logger.info("GET firestation -> getPersonsInFireStationArea /**/ HttpStatus " +
+                ": " + HttpStatus.OK + " /**/ Result : '{}'.", personsInFireStationArea.toString());
 
         return personsInFireStationArea;
     }
@@ -62,9 +72,14 @@ public class MainController {
 
         ChildrenByAddress childrenByAddress = personService.getChildrenByAddress(address);
         // if there is no person on this address or address don't exist send status and error message
-        if (childrenByAddress == null)
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "At this address " + address + " there is no persons or this address don't exist");
+        if (childrenByAddress == null) {
+            logger.error("GET childAlert -> getChildrenByAddress /**/ " +
+                    "HttpStatus : " + HttpStatus.NOT_FOUND + " /**/ Message : At this address " + address + " there is no persons or this address don't exist");
 
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "At this address " + address + " there is no persons or this address don't exist");
+        }
+        logger.info("GET childAlert -> getChildrenByAddress /**/ HttpStatus " +
+                ": " + HttpStatus.OK + " /**/ Result : '{}'.", childrenByAddress.toString());
         return childrenByAddress;
     }
 
@@ -79,8 +94,13 @@ public class MainController {
 
         List<String> phoneNumbersInFireStationArea = fireStationService.getPhoneNumbersInFireStationArea(stationNumber);
         // if there is no station with this id or list is empty send status and error message
-        if (phoneNumbersInFireStationArea == null || phoneNumbersInFireStationArea.isEmpty())
+        if (phoneNumbersInFireStationArea == null || phoneNumbersInFireStationArea.isEmpty()){
+            logger.error("GET phoneAlert -> getPhoneNumbersInFireStationArea /**/ " +
+                    "HttpStatus : " + HttpStatus.NOT_FOUND + " /**/ Message : Fire station with this id " + stationNumber + " don't exist or there is no persons on this area");
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Fire station with this id " + stationNumber + " don't exist or there is no persons on this area");
+        }
+        logger.info("GET phoneAlert -> getPhoneNumbersInFireStationArea /**/ HttpStatus " +
+                ": " + HttpStatus.OK + " /**/ Result : '{}'.", phoneNumbersInFireStationArea.toString());
 
         return phoneNumbersInFireStationArea;
     }
@@ -96,9 +116,14 @@ public class MainController {
 
         PersonsAndStationByAddress personsAndStationByAddress = personService.getPersonsAndStationByAddress(address);
         // if there is no persons or the address is wrong send status and error message
-        if (personsAndStationByAddress == null)
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "At this address " + address + " there is no persons or address is wrong");
+        if (personsAndStationByAddress == null){
+            logger.error("GET fire -> getPersonsAndStationByAddress /**/ " +
+                    "HttpStatus : " + HttpStatus.NOT_FOUND + " /**/ Message : At this address " + address + " there is no persons or address is wrong");
 
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "At this address " + address + " there is no persons or address is wrong");
+        }
+        logger.info("GET fire -> getPersonsAndStationByAddress /**/ HttpStatus " +
+                ": " + HttpStatus.OK + " /**/ Result : '{}'.", personsAndStationByAddress.toString());
         return personsAndStationByAddress;
     }
 
@@ -115,7 +140,14 @@ public class MainController {
 
         // if there is no persons or station ids not exist send status and error message
         if (personsAndAddressesByStation == null || personsAndAddressesByStation.isEmpty())
+        {
+            logger.error("GET flood/stations -> getPersonsAndAddressesByStations /**/ " +
+                    "HttpStatus : " + HttpStatus.NOT_FOUND + " /**/ Message : Fire stations with this ids " + stations + " don't exist or there is no persons on this area");
+
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Fire stations with this ids " + stations + " don't exist or there is no persons on this area");
+        }
+        logger.info("GET flood/stations -> getPersonsAndAddressesByStations /**/ HttpStatus " +
+                ": " + HttpStatus.OK + " /**/ Result : '{}'.", personsAndAddressesByStation.toString());
 
         return personsAndAddressesByStation;
     }
@@ -132,8 +164,14 @@ public class MainController {
 
         List<PersonInfo> personsInfo = personService.getPersonsInfo(firstName, lastName);
         // if there is no person searched send status and error message
-        if (personsInfo == null || personsInfo.isEmpty())
+        if (personsInfo == null || personsInfo.isEmpty()){
+            logger.error("GET personInfo -> getPersonsInfo /**/ " +
+                    "HttpStatus : " + HttpStatus.NOT_FOUND + " /**/ Message : There is no persons with this name: " + lastName + " " + firstName + " at this address");
+
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "There is no persons with this name: " + lastName + " " + firstName + " at this address");
+        }
+        logger.info("GET personInfo -> getPersonsInfo /**/ HttpStatus " +
+                ": " + HttpStatus.OK + " /**/ Result : '{}'.", personsInfo.toString());
 
         return personsInfo;
     }
@@ -145,12 +183,18 @@ public class MainController {
      * @return List of email addresses
      */
     @GetMapping(path = "communityEmail")
-    public List<String> getPersonsInfo(@RequestParam() String city) {
+    public List<String> getCommunityEmails(@RequestParam() String city) {
 
         List<String> communityEmails = personService.getCommunityEmails(city);
         // if there is no email or city don't exist send status and error message
-        if (communityEmails == null || communityEmails.isEmpty())
+        if (communityEmails == null || communityEmails.isEmpty()){
+            logger.error("GET communityEmail -> getCommunityEmails /**/ " +
+                    "HttpStatus : " + HttpStatus.NOT_FOUND + " /**/ Message : There is no persons living in this city: " + city + " or this city don't exist");
+
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "There is no persons living in this city: " + city + " or this city don't exist");
+        }
+        logger.info("GET communityEmail -> getCommunityEmails /**/ HttpStatus " +
+                ": " + HttpStatus.OK + " /**/ Result : '{}'.", communityEmails.toString());
 
         return communityEmails;
     }

f6f4f1dcdb2c3323550668c7ccef0d3e0af988c9
dlugi57 <dlugoszpeter@gmail.com>
2020-09-28 22:02:50 +0200

    TEST first test and configuration

--- src/test/java/com/safetynet/alerts/controller/PersonControllerTest.java
+++ src/test/java/com/safetynet/alerts/controller/PersonControllerTest.java
@@ -1,4 +1,63 @@
 package com.safetynet.alerts.controller;
 
-public class PersonControllerTest {
+import com.safetynet.alerts.model.Person;
+import com.safetynet.alerts.service.PersonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@SpringBootTest
+//@AutoConfigureMockMvc
+@WebMvcTest(PersonController.class)
+class PersonControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PersonService service;
+
+
+    @Test
+    void getPersonByFirstNameAndLastName() throws Exception {
+
+        when(service.getPersonByFirstNameAndLastName(anyString(),
+                anyString())).thenReturn(new Person("John", "Boyd",
+                "1509 Culver St", "Culver", "97451",
+                "841-874-6512", "jaboyd@email.com"));
+
+        this.mockMvc.perform(get("/person")
+                .param("lastName", "Boyd")
+                .param("firstName", "John")
+                .contentType("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.lastName").value("Boyd"))
+                .andExpect(jsonPath("$.firstName").value("John"));
+
+
+    }
+
+    @Test
+    void addPerson() {
+    }
+
+    @Test
+    void updatePerson() {
+    }
+
+    @Test
+    void deletePerson() {
+    }
+
+    @Test
+    void getPersons() {
+    }
 }

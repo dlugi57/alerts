58f9687fe9c3fb03e1326b867a1975756cefe540
dlugi57 <dlugoszpeter@gmail.com>
2020-10-03 00:06:30 +0200

    TEST & CLEAN

--- src/test/java/com/safetynet/alerts/controller/MainControllerTest.java
+++ src/test/java/com/safetynet/alerts/controller/MainControllerTest.java
@@ -16,8 +16,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -34,29 +33,6 @@ class MainControllerTest {
     private FireStationService fireStationService;
     @MockBean
     private PersonService personService;
-
-    public static List<Person> persons = new ArrayList<>();
-
-    static {
-        persons.add(new Person("John", "Boyd", "1509 Culver St",
-                "Culver", "97451", "841-874-6512", "jaboyd@email.com"));
-        persons.add(new Person("Johnathan", "Barrack", "29 15th St",
-                "Culver", "97451", "841-874-6513", "drk@email.com"));
-        persons.add(new Person("Doc", "Spring",
-                "1515 Java St - Beverly Hills", "Los Angeles", "90211",
-                "123-456-7890", "Doc.Spring@email.com"));
-        persons.add(new Person("Tenley", "Boyd", "1509 Culver St",
-                "Culver", "97451", "841-874-6512", "tenz@email.com"));
-        persons.add(new Person("Tenley", "Boyd", "1509 Culver St",
-                "Culver", "97451", "841-874-6515", "tenz@email.com"));
-        persons.add(new Person("Jacob", "Boyd", "1509 Culver St",
-                "Culver", "97451", "841-874-6513", "drk@email.com"));
-        persons.add(new Person("Roger", "Boyd", "1509 Culver St",
-                "Culver", "97451", "841-874-6512", "jaboyd@email.com"));
-        persons.add(new Person("Felicia", "Boyd", "1509 Culver St",
-                "Culver", "97451", "841-874-6544", "jaboyd@email.com"));
-    }
-
 
     public static List<PersonFireStation> personsFireStation = new ArrayList<>();
 
@@ -203,15 +179,15 @@ class MainControllerTest {
     // TODO: 02/10/2020 how to create array in this example
     static {
         personsFire.add(new PersonFire("Boyd", "841-874-6512", 36, Arrays.asList("aznol:350mg", "hydrapermazol:100mg"),
-                Arrays.asList("nillacilan")));
+                Collections.singletonList("nillacilan")));
         personsFire.add(new PersonFire("Boyd", "841-874-6513", 31, Arrays.asList("pharmacol:5000mg", "terazine:10mg", "noznazol:250mg"),
-                Arrays.asList()));
-        personsFire.add(new PersonFire("Boyd", "841-874-6512", 8, Arrays.asList(),
-                Arrays.asList("peanut")));
-        personsFire.add(new PersonFire("Boyd", "841-874-6512", 3, Arrays.asList(),
-                Arrays.asList()));
-        personsFire.add(new PersonFire("Boyd", "841-874-6544", 34, Arrays.asList("tetracyclaz:650mg"),
-                Arrays.asList("xilliathal")));
+                Collections.emptyList()));
+        personsFire.add(new PersonFire("Boyd", "841-874-6512", 8, Collections.emptyList(),
+                Collections.singletonList("peanut")));
+        personsFire.add(new PersonFire("Boyd", "841-874-6512", 3, Collections.emptyList(),
+                Collections.emptyList()));
+        personsFire.add(new PersonFire("Boyd", "841-874-6544", 34, Collections.singletonList("tetracyclaz:650mg"),
+                Collections.singletonList("xilliathal")));
     }
 
     static int station = 3;
@@ -244,15 +220,183 @@ class MainControllerTest {
                 .andExpect(status().isNotFound());
     }
 
-    @Test
-    void getPersonsAndAddressesByStations() throws Exception {
+
+    static String address = "644 Gershwin Cir";
+    static String address2 = "908 73rd St";
+
+    public static PersonsAndAddressesByStation personsAndAddressesByStation1 =
+            new PersonsAndAddressesByStation(address, personsFire);
+    public static PersonsAndAddressesByStation personsAndAddressesByStation2 =
+            new PersonsAndAddressesByStation(address2, personsFire);
+
+    public static List<PersonsAndAddressesByStation> personsAndAddressesByStationList =
+            new ArrayList<>();
+
+    static {
+        personsAndAddressesByStationList.add(personsAndAddressesByStation1);
+        personsAndAddressesByStationList.add(personsAndAddressesByStation2);
+
     }
 
     @Test
+    void getPersonsAndAddressesByStations() throws Exception {
+        when(personService.getPersonsAndAddressesByStations(anyList())).thenReturn(personsAndAddressesByStationList);
+
+        this.mockMvc.perform(get("/flood/stations")
+                .param("stations", "1", "2")
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$.[0].persons", hasSize(5)))
+                .andExpect(jsonPath("$.[0].address").value("644 Gershwin Cir"))
+                .andExpect(jsonPath("$.[0].persons[0].lastName").value("Boyd"));
+    }
+
+    @Test
+    void getPersonsAndAddressesByStations_Empty() throws Exception {
+        when(personService.getPersonsAndAddressesByStations(anyList())).thenReturn(Collections.emptyList());
+
+        this.mockMvc.perform(get("/flood/stations")
+                .param("stations", "1", "2")
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getPersonsAndAddressesByStations_NotFound() throws Exception {
+        when(personService.getPersonsAndAddressesByStations(anyList())).thenReturn(null);
+
+        this.mockMvc.perform(get("/flood/stations")
+                .param("stations", "1", "2")
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+
+    public static List<PersonInfo> personsInfo = new ArrayList<>();
+
+    static {
+        personsInfo.add(new PersonInfo("Boyd", "1509 Culver St", 36, "jaboyd@email.com",
+                Arrays.asList("aznol:350mg", "hydrapermazol:100mg"),
+                Collections.singletonList("nillacilan")));
+        personsInfo.add(new PersonInfo("Boyd", "1509 Culver St", 31, "jaboyd@email.com", Arrays.asList("pharmacol:5000mg", "terazine:10mg", "noznazol:250mg"),
+                Collections.emptyList()));
+        personsInfo.add(new PersonInfo("Boyd", "1509 Culver St", 8, "jaboyd@email.com", Collections.emptyList(),
+                Collections.singletonList("peanut")));
+        personsInfo.add(new PersonInfo("Boyd", "1509 Culver St", 3, "jaboyd@email.com", Collections.emptyList(),
+                Collections.emptyList()));
+        personsInfo.add(new PersonInfo("Boyd", "1509 Culver St", 34, "jaboyd@email.com", Collections.singletonList("tetracyclaz:650mg"),
+                Collections.singletonList("xilliathal")));
+    }
+
+
+    @Test
     void getPersonsInfo() throws Exception {
+        when(personService.getPersonsInfo(anyString(), anyString())).thenReturn(personsInfo);
+
+        this.mockMvc.perform(get("/personInfo")
+                .param("firstName", "John")
+                .param("lastName", "Boyd")
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(5)))
+                .andExpect(jsonPath("$.[0].lastName").value("Boyd"))
+                .andExpect(jsonPath("$.[0].address").value("1509 Culver St"));
+    }
+
+    @Test
+    void getPersonsInfo_Empty() throws Exception {
+        when(personService.getPersonsInfo(anyString(), anyString())).thenReturn(Collections.emptyList());
+
+        this.mockMvc.perform(get("/personInfo")
+                .param("firstName", "John")
+                .param("lastName", "Boyd")
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getPersonsInfo_NotFound() throws Exception {
+        when(personService.getPersonsInfo(anyString(), anyString())).thenReturn(null);
+
+        this.mockMvc.perform(get("/personInfo")
+                .param("firstName", "John")
+                .param("lastName", "Boyd")
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    public static List<Person> persons = new ArrayList<>();
+
+    static {
+        persons.add(new Person("John", "Boyd", "1509 Culver St",
+                "Culver", "97451", "841-874-6512", "jaboyd@email.com"));
+        persons.add(new Person("Johnathan", "Barrack", "29 15th St",
+                "Culver", "97451", "841-874-6513", "drk@email.com"));
+        persons.add(new Person("Doc", "Spring",
+                "1515 Java St - Beverly Hills", "Los Angeles", "90211",
+                "123-456-7890", "Doc.Spring@email.com"));
+        persons.add(new Person("Tenley", "Boyd", "1509 Culver St",
+                "Culver", "97451", "841-874-6512", "tenz@email.com"));
+        persons.add(new Person("Tenley", "Boyd", "1509 Culver St",
+                "Culver", "97451", "841-874-6515", "tenz@email.com"));
+        persons.add(new Person("Jacob", "Boyd", "1509 Culver St",
+                "Culver", "97451", "841-874-6513", "drk@email.com"));
+        persons.add(new Person("Roger", "Boyd", "1509 Culver St",
+                "Culver", "97451", "841-874-6512", "jaboyd@email.com"));
+        persons.add(new Person("Felicia", "Boyd", "1509 Culver St",
+                "Culver", "97451", "841-874-6544", "jaboyd@email.com"));
+    }
+
+    public static List<String> communityEmails = new ArrayList<>();
+
+    static {
+        for (Person person : persons) {
+            if (person.getCity().equals("Culver")) {
+                communityEmails.add(person.getEmail());
+            }
+        }
     }
 
     @Test
     void getCommunityEmails() throws Exception {
+        when(personService.getCommunityEmails(anyString())).thenReturn(communityEmails);
+
+        this.mockMvc.perform(get("/communityEmail")
+                .param("city", "Culver")
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(7)))
+                .andExpect(jsonPath("$.[0]").value("jaboyd@email.com"))
+                .andExpect(jsonPath("$.[1]").value("drk@email.com"));
+    }
+
+    @Test
+    void getCommunityEmails_Empty() throws Exception {
+        when(personService.getCommunityEmails(anyString())).thenReturn(Collections.emptyList());
+
+        this.mockMvc.perform(get("/communityEmail")
+                .param("city", "Culver")
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void getCommunityEmailsNotFound() throws Exception {
+        when(personService.getCommunityEmails(anyString())).thenReturn(null);
+
+        this.mockMvc.perform(get("/communityEmail")
+                .param("city", "Culver")
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
 }
